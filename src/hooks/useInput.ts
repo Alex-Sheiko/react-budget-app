1d75a73947179d1e836e27ac46399b582f8dfe55
Alex Sheiko <82119411+by-ultra-dev@users.noreply.github.com>
2023-04-13 11:30:46 +0300

    refactor(p_4): add useCallback for useInput, useToggle hooks

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,10 +1,11 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useCallback, useState } from "react";
 
 export const useInput = (initialValue = "") => {
   const [value, setValue] = useState(initialValue);
 
-  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const onChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
-  };
+  }, []);
+
   return { value, onChange };
 };
--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export const useToggle = (initialValue: boolean = false): [value: boolean, toogle: () => void] => {
   const [value, setValue] = useState<boolean>(initialValue);
 
-  const toggleValue = () => setValue((prevValue) => !prevValue);
+  const toggleValue = useCallback(() => {
+    setValue((prevValue) => !prevValue);
+  }, []);
 
   return [value, toggleValue];
 };
